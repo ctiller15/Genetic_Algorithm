607134b1e16d86ed008f4e3568dbcada93f42239
Tiller <ctiller@proforma.com>
2018-05-03 21:25:56 -0400

    Create population class

---
+++ Genetic_Algorithm/Population.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genetic_Algorithm
+{
+    class Population
+    {
+        //
+        static public int PopSize = 100;
+        public int fittest = 0;
+        public string fittestStr = "";
+        public Individual[] individuals = new Individual[PopSize];
+
+        // Initialize population
+        public void InitializePopulation(int size)
+        {
+            for (int i = 0; i < individuals.Count(); i++)
+            {
+                individuals[i] = new Individual();
+            }
+        }
+
+        // Get the fittest individual(s)
+        public IEnumerable<Individual>[] GetFittestIndividuals(int percentage)
+        {
+            // Sort the individuals:
+            var fitIndividuals = new IEnumerable<Individual>[2];
+
+            var individualCount = percentage * individuals.Count() / 100;
+            // The fittest individuals
+            fitIndividuals[0] = individuals.OrderByDescending(o => o.fitness).Take(individualCount);
+            fittest = fitIndividuals[0].First().fitness;
+            fittestStr = fitIndividuals[0].First().genes.ToString();
+            // The least fit individuals
+            fitIndividuals[1] = individuals.OrderBy(o => o.fitness).Take(individualCount);
+
+            return fitIndividuals;
+        }
+
+        public void CalculateFitness()
+        {
+            // We calculate the fitness of each individual.
+            for(int i = 0; i < individuals.Count(); i++)
+            {
+                individuals[i].FitnessCalc();
+            }
+
+            // After calculating the fitness, we find the fittest individuals.
+            GetFittestIndividuals(10);
+        }
+
+    }
+}
