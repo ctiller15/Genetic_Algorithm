69c537f2cd1b91f0b76062acc789d9e53e29a480
Tiller <ctiller@proforma.com>
2018-05-03 20:52:51 -0400

    Create individual class

---
+++ Genetic_Algorithm/Individual.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genetic_Algorithm
+{
+    class Individual
+    {
+        public int fitness = 0;
+        static public int geneLength = 5;
+        public int[] genes = new int[geneLength];
+
+        // Calculates the fitness for this individual.
+        // If the gene value matches our desired binary, then the fitness increases.
+        public void FitnessCalc()
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                if(genes[i] == 1)
+                {
+                    fitness++;
+                }
+            }
+        }
+    }
+}
